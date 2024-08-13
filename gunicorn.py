16d4e6f5b292e61aea527dea5c7a7ada0fbaf76a
Kostin Ilya <ilyaspecial999@gmail.com>
2024-08-13 22:15:23 +0300

    1

--- gunicorn.py
+++ gunicorn.py
@@ -2,7 +2,8 @@
 
 bind = '0.0.0.0:443'
 max_requests = 1000
-workers = 6
+threads = 5
+workers = 5
 wsgi_app = 'coffeetox:app'
 keyfile = './coffeetox/ssl/privkey.pem'
 certfile = './coffeetox/ssl/fullchain.pem'
